9d5b26e9c3fc66f91ed0fe9c908737db738238b8
Install <kravmaxim@gmail.com>
2022-02-26 20:54:39 +0400

    lesson6

---
+++ src/main/java/lesson6/ExampleMain.java
@@ -0,0 +1,38 @@
+package lesson6;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import lesson6.db.dao.*;
+
+public class ExampleMain {
+
+    public static void main( String[] args ) throws IOException {
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new
+                SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+        db.dao.CategoriesMapper categoriesMapper = session.getMapper(db.dao.CategoriesMapper.class);
+        db.model.CategoriesExample example = new db.model.CategoriesExample();
+        example.createCriteria().andIdEqualTo(800);
+        List<db.model.Categories> list = categoriesMapper.selectByExample(example);
+
+        db.model.Categories categories = new db.model.Categories();
+        categories.setId(800);
+        categories.setTitle("123");
+        categoriesMapper.updateByExample(categories, example);
+
+
+        System.out.println(categoriesMapper.countByExample(example));
+
+        System.out.println(list);
+    }
+}
