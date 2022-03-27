cd8d445758919ad02b7a747e00a2af48866be826
Install <kravmaxim@gmail.com>
2022-03-27 19:29:03 +0400

    lesson3

---
+++ src/test/java/lesson3/LogMain.java
@@ -0,0 +1,27 @@
+package lesson3;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class LogMain {
+
+    @BeforeAll
+    static void setUp(){
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+    @Test
+    void getRecipeWithLoggingPositiveTest() {
+        given()
+                .log()
+                .all()
+                .when()
+                .get("https://api.spoonacular.com/recipes/716429/information")
+                .prettyPeek()
+                .then()
+                .statusCode(200);
+    }
+}
